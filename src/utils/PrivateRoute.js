23606b228ed11e44df730858949e98a239de6098
MinhHieu259 <mhieu7252@gmail.com>
2022-12-26 17:22:12 +0700

    new

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,7 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoute = ({children, ...rest}) => {
-    let auth = {'token': true}
+    let auth = {'token': true} //hihi
 
     return (
        auth.token ? <Outlet /> : <Navigate to="/"/>
